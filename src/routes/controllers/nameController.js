b656b56069c61b01c905c398ed2edb8bd8e5f69b
Your Name <you@example.com>
2024-11-10 17:22:37 +0700

    FIX

--- src/routes/controllers/nameController.js
+++
@@ -1,9 +0,0 @@
-const names = require('../models/names');
-
-const submitName = (req, res) => {
-    const name = req.body.name;
-    names.addName(name);
-    res.json({ message: `Xin chào, ${name}!`, names: names.getNames() });
-};
-
-module.exports = { submitName };
